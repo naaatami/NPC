6a6d19e4c22694bd999e0268410d577b0cc01596
Shane Burke <67300048+fabio-gustav@users.noreply.github.com>
2025-03-15 18:07:23 -0500

    File input works as intended

--- find3Color.java
+++ find3Color.java
@@ -13,22 +13,24 @@ public class find3Color {
             File inFile = new File("C:\\Users\\burke\\IT328\\findcolor\\findthreecolor\\src\\graphs2025.txt");
             System.out.println("File Read.");
             Scanner scan = new Scanner(inFile);
-            while (scan.hasNextLine()) {
+            while (scan.hasNext()) {
                 int vertices = scan.nextInt();
                 System.out.println("V: " + vertices);
-                graphs.add(new Graph(vertices));
-                scan.nextLine();
-                for (int i = 0;i<vertices;i++) {
-                    for (int j = i; j<vertices;j++) {
-                        if (scan.nextInt() == 1) {
-                            graphs.get(graphs.size()-1).addEdge(i,j);
-                        }
-                        // System.out.print(scan.next());
-                    }
+                if (vertices != 0) {
+                    graphs.add(new Graph(vertices));
                     scan.nextLine();
-                }
+                    for (int i = 0;i<vertices;i++) {
+                        for (int j = 0; j<vertices;j++) {
+                            if (scan.nextInt() == 1) {
+                                graphs.get(graphs.size()-1).addEdge(i,j);
+                            }
+                            // System.out.print(scan.next());
+                        }
+                        scan.nextLine();
+                    }
 
-                System.out.println(graphs.get(graphs.size()-1));
+                    System.out.println(graphs.get(graphs.size()-1));
+                }
             }
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found");
