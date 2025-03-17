c2aebc65883a94553c958e3b3d0c0b3c3152da3e
Bennett <112603507+bennettbDEV@users.noreply.github.com>
2025-03-16 22:40:00 -0500

    Swapping my problem to use helper methods

--- find3NAESAT.java
+++ find3NAESAT.java
@@ -32,8 +32,8 @@ public class find3NAESAT {
             int cnfCount = 1;
             // Iterate through every line and construct the 3CNF
             while (line != null) {
-                int[] cnfArray = splitInput(line);
-                int[] numTermsNumClauses = findNandK(cnfArray);
+                int[] cnfArray = Helper.splitCNFInput(line);
+                int[] numTermsNumClauses = Helper.findNandK(cnfArray);
                 int numTerms = numTermsNumClauses[0];
                 int numClauses = numTermsNumClauses[1];
 
@@ -64,7 +64,7 @@ public class find3NAESAT {
                 System.out.print(cert.substring(0, cert.length() - 1) + "]");
 
                 System.out.println();
-                System.out.println(generate3CNF(cnfArray) + " ==>");
+                System.out.println(Helper.format3CNF(cnfArray) + " ==>");
                 System.out.println(generateAssignments(cnfArray, baseAssignments));
                 System.out.println();
 
@@ -78,58 +78,6 @@ public class find3NAESAT {
             error.printStackTrace();
         }
 
-    }
-
-    // given a CNF clause, finds n and k and sends it back in an array.
-    // index 0 is n, index 1 is k
-    public static int[] findNandK(int[] cnfArray) {
-        int[] nk = { 0, 0 };
-        for (int currentNum : cnfArray) {
-            int value = Math.abs(currentNum);
-            if (value > nk[0]) {
-                nk[0] = value;
-            }
-            nk[1]++;
-        }
-        nk[1] = nk[1] / 3;
-        return nk;
-    }
-
-    // splits the 3CNF clause into a string array so it can be more easily processed
-    public static int[] splitInput(String inputString) {
-        String[] numberStringArray = inputString.trim().split("\\s+"); // separating by spaces
-
-        int[] numbers = new int[numberStringArray.length];
-        for (int i = 0; i < numberStringArray.length; i++) {
-            numbers[i] = Integer.parseInt(numberStringArray[i]); // Convert to int
-        }
-
-        return numbers;
-    }
-
-    public static String generate3CNF(int[] cnfArray) {
-
-        ArrayList<String> results = new ArrayList<String>();
-        for (int i = 0; i < cnfArray.length; i += 3) {
-            String group = "(";
-            if (cnfArray[i] < 0)
-                group += cnfArray[i];
-            else
-                group = group + " " + cnfArray[i];
-            group += "|";
-            if (cnfArray[i + 1] < 0)
-                group += cnfArray[i + 1];
-            else
-                group = group + " " + cnfArray[i + 1];
-            group += "|";
-            if (cnfArray[i + 2] < 0)
-                group += cnfArray[i + 2];
-            else
-                group = group + " " + cnfArray[i + 2];
-            group += ")";
-            results.add(group);
-        }
-        return String.join("/\\", results);
     }
 
     public static String generateAssignments(int[] cnfArray, Hashtable<Integer, Boolean> assignments) {
